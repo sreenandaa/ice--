e059456f72df21d73bc6669d7db5f36d4e81405b
Niharika Hareendran <niharikahareendran0@gmail.com>
2026-09-21 15:20:05 +0530

    Initial commit

---
+++ intercollege/src/test/java/com/example/intercollege/IntercollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intercollege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntercollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
